63897e7ca4463d27e600d3984f478b114e0f5635
WADA Hiroki <hiroki.wada@crisp.co.jp>
2025-05-15 14:31:00 +0900

    feat: Google Analyticsの導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
+import { initGoogleAnalytics } from './config/analytics'
+
+// Google Analyticsの初期化
+initGoogleAnalytics()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
